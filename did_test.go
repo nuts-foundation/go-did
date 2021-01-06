3f8ebcdacff769b8ff93002dfa6d9ea559bf1707
stevenvegt <steven.vandervegt@nedap.com>
2021-01-06 11:54:58 +0100

    Improved JSON (un)marshalling
    changed main package to godid
    marshal per type
    normalize did doc before unmarshal
    make use of ockam-network/did for did parsing
    removed dependency on testify framework
    Added simple service type (needs more work)

--- did_test.go
+++ did_test.go
@@ -1,98 +1,53 @@
-package main
+package didparser
 
 import (
 	"encoding/json"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	ockamDid "github.com/ockam-network/did"
 )
 
-func TestParseDIDDocument(t *testing.T) {
-	input := `{
-  "@context": [
-    "https://www.w3.org/ns/did/v1"
-  ],
-  "id": "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff",
-  "controller": [
-    "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff",
-    "did:nuts:f03a00f1-9615-4060-bd00-bd282e150c46"
-  ],
-  "verificationMethod": [
-    {
-      "id": "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff#key-1",
-      "type": "JsonWebKey2020",
-      "controller": "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff",
-      "publicKeyJwk": {
-        "kty" : "EC",
-		"crv" : "P-256",
-		"x"   : "SVqB4JcUD6lsfvqMr-OKUNUphdNn64Eay60978ZlL74",
-		"y"   : "lf0u0pMj4lGAzZix5u4Cm5CMQIgMNpkwy163wtKYVKI"
-  	  }
-    },
-    {
-      "id": "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff#key-2",
-      "type": "JsonWebKey2020",
-      "controller": "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff",
-      "publicKeyJwk": {
-        "kty" : "EC",
-		"crv" : "P-256",
-		"x"   : "SVqB4JcUD6lsfvqMr-OKUNUphdNn64Eay60978ZlL74",
-		"y"   : "lf0u0pMj4lGAzZix5u4Cm5CMQIgMNpkwy163wtKYVKI"
-  	  }
-    }
-  ],
-  "authentication": [
-    "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff#key-1",
-    "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff#key-2"
-  ],
-  "assertionMethod": [
-    "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff#key-1"
-  ],
-  "service": [
-    {
-      "id": "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff#service-1",
-      "type": "nuts:bolt:eoverdracht",
-      "serviceEndpoint": "did:nuts:<vendor>#service-76"
-    },
-    {
-      "id": "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff#service-2",
-      "type": "nuts:core:consent",
-      "serviceEndpoint": "did:nuts:<vendor>#service-2"
-    }
-  ]
-}`
+func TestDID_UnmarshalJSON(t *testing.T) {
+	jsonTestSting := `"did:nuts:123"`
 
-	var actual didDocument
-	if err := json.Unmarshal([]byte(input), &actual); err != nil {
-		t.Error(err)
+	did := DID{}
+	err := json.Unmarshal([]byte(jsonTestSting), &did)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
 		return
 	}
-	t.Run("validate ID", func(t *testing.T) {
-		assert.Equal(t, "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff", actual.ID.String())
-	})
-	t.Run("validate context", func(t *testing.T) {
-		assert.Len(t, actual.Context, 1)
-		assert.Equal(t, "https://www.w3.org/ns/did/v1", actual.Context[0].String())
-	})
-	t.Run("validate controllers", func(t *testing.T) {
-		assert.Len(t, actual.Controllers, 2)
-		assert.Equal(t, "did:nuts:04cf1e20-378a-4e38-ab1b-401a5018c9ff", actual.Controllers[0].String())
-		assert.Equal(t, "did:nuts:f03a00f1-9615-4060-bd00-bd282e150c46", actual.Controllers[1].String())
-	})
-
+	if did.Method != "nuts" {
+		t.Errorf("expected nuts got %s", did.Method)
+		return
+	}
 }
 
-func TestDIDList_UnmarshalJSON(t *testing.T) {
-	input := `{
-	  "id": "<nuts did id>",
-	  "controller": "ctrl1",
-	  "verificationMethod": [],
-	  "authentication": [],
-	  "service":[]
-	}`
-	var actual didDocument
-	if err := json.Unmarshal([]byte(input), &actual); err != nil {
-		t.Error(err)
+func TestDID_MarshalJSON(t *testing.T) {
+	wrappedDid, err := ockamDid.Parse("did:nuts:123")
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+		return
+	}
+	did := DID{*wrappedDid}
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+		return
+	}
+	result, err := json.Marshal(did)
+	if string(result) != `"did:nuts:123"` {
+		t.Errorf("expected \"did:nuts:123\" got: %s", result)
+	}
+}
+
+func TestParseDID(t *testing.T) {
+	did, err := ParseDID("did:nuts:123")
+
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
 		return
 	}
 
+	if did.String() != "did:nuts:123" {
+		t.Errorf("expected parsed did to be 'did:nuts:123', got: %s", did.String())
+	}
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
-module github.com/nuts-foundation/nutsdid-method
+module github.com/nuts-foundation/golang-didparser
 
 go 1.15
 
 require (
-	github.com/piprate/json-gold v0.3.0 // indirect
-	github.com/stretchr/testify v1.6.1
 	github.com/lestrrat-go/jwx v1.0.5
+	github.com/ockam-network/did v0.1.3
+	github.com/stretchr/testify v1.5.1
 )
