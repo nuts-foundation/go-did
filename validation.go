969ebe1ac13f3286326e04f58affdf1e7bd8f412
Rein Krul <info@reinkrul.nl>
2021-03-12 08:45:32 +0100

    godoc

--- validation.go
+++ validation.go
@@ -5,10 +5,15 @@ import (
 	"fmt"
 )
 
+// ErrDIDDocumentInvalid indicates DID Document validation failed
 var ErrDIDDocumentInvalid = errors.New("DID Document validation failed")
+// ErrInvalidContext indicates the DID Document's `@context` is invalid
 var ErrInvalidContext = errors.New("invalid context")
+// ErrInvalidID indicates the DID Document's `id` is invalid
 var ErrInvalidID = errors.New("invalid ID")
 
+// ValidateDocument validates the given DID Document. If validation fails, it returns an error describing what's invalid,
+// wrapped in ErrDIDDocumentInvalid.
 func ValidateDocument(document Document) error {
 	validationError := func(validationErr error) error {
 		return fmt.Errorf("%s: %w", ErrDIDDocumentInvalid.Error(), validationErr)
