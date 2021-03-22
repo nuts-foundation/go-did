e80c70d98476cc83eeb6789bd073a398f3c9b32e
Rein Krul <info@reinkrul.nl>
2021-03-22 14:14:37 +0100

    PR feedback

--- did/validator.go
+++ did/validator.go
@@ -3,14 +3,32 @@ package did
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
 // ErrDIDDocumentInvalid indicates DID Document validation failed
-var ErrDIDDocumentInvalid = errors.New("DID Document validation failed")
+var ErrDIDDocumentInvalid = validationError{}
+
 // ErrInvalidContext indicates the DID Document's `@context` is invalid
 var ErrInvalidContext = errors.New("invalid context")
+
 // ErrInvalidID indicates the DID Document's `id` is invalid
 var ErrInvalidID = errors.New("invalid ID")
+
+// ErrInvalidController indicates the DID Document's `controller` is invalid
+var ErrInvalidController = errors.New("invalid controller")
+
+// ErrInvalidVerificationMethod indicates the verificationMethod is invalid (e.g. invalid `id` or `type`)
+var ErrInvalidVerificationMethod = errors.New("invalid verificationMethod")
+
+// ErrInvalidAuthentication indicates the authentication is invalid (e.g. invalid `id` or `type`)
+var ErrInvalidAuthentication = errors.New("invalid authentication")
+
+// ErrInvalidAssertionMethod indicates the assertion method is invalid (e.g. invalid `id` or `type`)
+var ErrInvalidAssertionMethod = errors.New("invalid assertionMethod")
+
+// ErrInvalidService indicates the service is invalid (e.g. invalid `id` or `type`)
+var ErrInvalidService = errors.New("invalid service")
 
 // Validator defines functions for validating a DID document.
 type Validator interface {
@@ -34,17 +52,109 @@ func (m MultiValidator) Validate(document Document) error {
 
 // W3CSpecValidator validates a DID document according to the W3C DID Core Data Model specification (https://www.w3.org/TR/did-core/).
 type W3CSpecValidator struct {
-
 }
 
 func (w W3CSpecValidator) Validate(document Document) error {
-	// Verify @context
+	return MultiValidator{[]Validator{
+		baseValidator{},
+		verificationMethodValidator{},
+		verificationMethodRelationshipValidator{
+			getter: func(document Document) VerificationRelationships {
+				return document.Authentication
+			},
+			err: ErrInvalidAuthentication,
+		},
+		verificationMethodRelationshipValidator{
+			getter: func(document Document) VerificationRelationships {
+				return document.AssertionMethod
+			},
+			err: ErrInvalidAssertionMethod,
+		},
+		serviceValidator{},
+	}}.Validate(document)
+}
+
+// baseValidator validates simple top-level DID document properties (@context, ID, controller)
+type baseValidator struct{}
+
+func (w baseValidator) Validate(document Document) error {
+	// Verify `@context`
 	if !containsContext(document, DIDContextV1) {
-		return validationError(ErrInvalidContext)
+		return makeValidationError(ErrInvalidContext)
 	}
-	// Verify ID
+	// Verify `id`
 	if document.ID.Empty() {
-		return validationError(ErrInvalidID)
+		return makeValidationError(ErrInvalidID)
+	}
+	// Verify `controller`
+	for _, controller := range document.Controller {
+		if controller.Empty() {
+			return makeValidationError(ErrInvalidController)
+		}
+	}
+	return nil
+}
+
+type verificationMethodValidator struct{}
+
+func (v verificationMethodValidator) Validate(document Document) error {
+	for _, vm := range document.VerificationMethod {
+		if !validateVM(vm) {
+			return makeValidationError(ErrInvalidVerificationMethod)
+		}
+	}
+	return nil
+}
+
+type verificationMethodRelationshipValidator struct {
+	getter func(document Document) VerificationRelationships
+	err    error
+}
+
+func (v verificationMethodRelationshipValidator) Validate(document Document) error {
+	for _, vm := range v.getter(document) {
+		if !validateVM(vm.VerificationMethod) {
+			return makeValidationError(v.err)
+		}
+	}
+	return nil
+}
+
+func validateVM(vm *VerificationMethod) bool {
+	if vm.ID.Empty() {
+		return false
+	}
+	if len(strings.TrimSpace(string(vm.Type))) == 0 {
+		return false
+	}
+	if vm.Controller.Empty() {
+		return false
+	}
+	return true
+}
+
+type serviceValidator struct{}
+
+func (s serviceValidator) Validate(document Document) error {
+	for _, service := range document.Service {
+		if len(strings.TrimSpace(service.ID.String())) == 0 {
+			return makeValidationError(ErrInvalidService)
+		}
+		if len(strings.TrimSpace(service.Type)) == 0 {
+			return makeValidationError(ErrInvalidService)
+		}
+		if service.ServiceEndpoint == nil {
+			return makeValidationError(ErrInvalidService)
+		}
+		switch x := service.ServiceEndpoint.(type) {
+		case string:
+		case map[string]interface{}:
+		case []interface{}:
+			_ = x // We need to 'do' something with 'x'
+			break
+		default:
+			return makeValidationError(ErrInvalidService)
+		}
 	}
 	return nil
 }
@@ -58,6 +168,23 @@ func containsContext(document Document, ctx string) bool {
 	return false
 }
 
-func validationError(validationErr error) error {
-	return fmt.Errorf("%s: %w", ErrDIDDocumentInvalid.Error(), validationErr)
+func makeValidationError(validationErr error) error {
+	return validationError{cause: validationErr}
+}
+
+type validationError struct {
+	cause error
+}
+
+func (v validationError) Unwrap() error {
+	return v.cause
+}
+
+func (v validationError) Is(err error) bool {
+	_, is := err.(validationError)
+	return is
+}
+
+func (v validationError) Error() string {
+	return fmt.Sprintf("DID Document validation failed: %v", v.cause)
 }
