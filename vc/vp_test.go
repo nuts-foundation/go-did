d6ecdcfe98506446c8b6ea7f528c6ccf76b3087d
reinkrul <info@reinkrul.nl>
2023-12-05 13:12:39 +0100

    Revert removal of JSON-LD array compacting (#102)

--- vc/vp_test.go
+++ vc/vp_test.go
@@ -65,8 +65,36 @@ func TestVerifiablePresentation_MarshalJSON(t *testing.T) {
 			if !assert.NoError(t, err) {
 				return
 			}
-			assert.Contains(t, string(bytes), "\"proof\":[{")
-			assert.Contains(t, string(bytes), "\"verifiableCredential\":[{")
+			assert.Contains(t, string(bytes), "\"proof\":{")
+			assert.Contains(t, string(bytes), "\"verifiableCredential\":{")
+		})
+		t.Run("ok - multiple credential and proof", func(t *testing.T) {
+			input := VerifiablePresentation{
+				VerifiableCredential: []VerifiableCredential{
+					{
+						Type: []ssi.URI{VerifiableCredentialTypeV1URI()},
+					},
+					{
+						Type: []ssi.URI{VerifiableCredentialTypeV1URI()},
+					},
+				},
+				Proof: []interface{}{
+					JSONWebSignature2020Proof{
+						Jws: "",
+					},
+					JSONWebSignature2020Proof{
+						Jws: "",
+					},
+				},
+			}
+
+			bytes, err := json.Marshal(input)
+
+			if !assert.NoError(t, err) {
+				return
+			}
+			assert.Contains(t, string(bytes), "\"proof\":[")
+			assert.Contains(t, string(bytes), "\"verifiableCredential\":[")
 		})
 	})
 
